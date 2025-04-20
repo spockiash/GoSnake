518c292b9b201cfa4b9f57b1d63dc37bb79bccf5
Bobo <spocky@post.cz>
2025-04-20 18:05:00 +0200

    added test render

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
@@ -15,15 +16,19 @@ func main() {
 
 	// Use fixed size layout instead of Flex
 	grid := tview.NewGrid().
-		SetRows(1, 40, 0).    // top padding, arena height, bottom padding
-		SetColumns(0, 80, 0). // left padding, arena width, right padding
-		AddItem(statusBar, 0, 1, 1, 1, 1, 0, false).
-		AddItem(arena, 1, 1, 1, 1, 0, 0, true)
+		SetRows(1, 40). // top padding, arena height, bottom padding
+		SetColumns(80). // left padding, arena width, right padding
+		AddItem(statusBar, 0, 0, 1, 1, 1, 0, false).
+		AddItem(arena, 1, 0, 1, 1, 0, 0, true)
 
 	arena.SetRect(0, 0, 80, 40)
 	// Display initial size information
 	//x, y, width, height := arena.GetRect()
-	arena.SetText(createTestString())
+
+	// testing render function
+	test := createTestRender(2, 2, 'W')
+
+	arena.SetText(test)
 	//arena.SetText(fmt.Sprintf("Arena size: %dx%d\nPosition: %d,%d", width, height, x, y))
 
 	// Capture key events
@@ -71,6 +76,37 @@ func createStatusBar() *tview.Flex {
 	return flex
 }
 
+// testing rendering of single symbol to arbitrary position
+func createTestRender(row int, col int, symbol rune) string {
+	height := ArenaHeight()
+	width := ArenaWidth()
+
+	lines := make([]string, height)
+
+	// iterate over height (rows of lines)
+	for h := 0; h < height; h++ {
+		line := make([]rune, ArenaWidth())
+		// iterate over width (line)
+		for w := 0; w < width; w++ {
+			// match to position
+			if h == row && w == col {
+				line[w] = symbol
+			}
+		}
+		lines[h] = string(line)
+	}
+
+	// construct output
+	var sb strings.Builder
+
+	for i := range lines {
+		sb.WriteString(lines[i])
+		sb.WriteRune('\n')
+	}
+
+	return sb.String()
+}
+
 func createTestString() string {
 	runes := make([]rune, ArenaWidth())
 
@@ -80,6 +116,7 @@ func createTestString() string {
 			counter = 0
 		}
 		runes[i] = rune('A' + counter) // adds A, B, C, D
+
 		counter++
 	}
 
